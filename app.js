494a3afb8ba74c31fccb90a0f4eeb227c9367669
Dane85 <wattsd1985@gmail.com>
2022-06-04 22:52:43 -0400

    Setup

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-});
+    res.render('pages/index')
+})
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`)
+})
