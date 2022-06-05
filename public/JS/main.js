85ee79521d604240ee1bc0eb07727fb016d69d96
Dane85 <wattsd1985@gmail.com>
2022-06-05 00:13:00 -0400

    Clean Up

---
+++ public/JS/main.js
@@ -0,0 +1,15 @@
+
+//Date
+function getDate() {
+    const today = new Date();
+
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+
+    let day = today.toLocaleDateString('en-US', options);
+    
+    return day;
+}
